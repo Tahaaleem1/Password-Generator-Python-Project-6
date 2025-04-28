9f71190076365978f5299eca7dbf75c19a95e80c
Taha <156198811+Tahaaleem1@users.noreply.github.com>
2025-04-28 15:43:47 +0500

    Update Password_Generator.py

--- Password_Generator.py
+++ Password_Generator.py
@@ -5,7 +5,7 @@
     "colab": {
       "provenance": [],
       "authorship_tag": "ABX9TyPFcJFhSRHwVeA32IYNtdCJ",
-      "include_colab_link": true
+      "include_colab_link": True
     },
     "kernelspec": {
       "name": "python3",
